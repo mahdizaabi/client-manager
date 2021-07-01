7b383f90a488a4a8a1b1af2f27f92a927bd47627
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-01 16:48:12 +0200

    multimodule maven project refactoring

--- client-manager-web/src/main/java/com/clientmanager/ClientManagerApplication.java
+++ client-manager-web/src/main/java/com/clientmanager/ClientManagerApplication.java
@@ -1,11 +1,9 @@
 package com.clientmanager;
 
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClientManagerApplication {
-
     public static void main(String[] args) {
 
     }
