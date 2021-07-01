b3372ec97aff7c4fcb825abb206566949b8817ae
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-01 13:07:20 +0200

    project initialization

---
+++ src/main/java/com/clientmanager/ClientManagerApplication.java
@@ -0,0 +1,14 @@
+package com.clientmanager;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientManagerApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+ 
---
+++ src/test/java/com/clientmanager/ClientManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
