a2d3b457db7c48fc0fd684868160fcfd4bb234d2
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-03 23:35:39 +0200

    add visit entity

--- client-manager-data/src/main/java/com/clientmanager/model/PetType.java
+++ client-manager-data/src/main/java/com/clientmanager/model/PetType.java
@@ -1,6 +1,6 @@
 package com.clientmanager.model;
 
-public class PetType extends BaseEntity{
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
---
+++ client-manager-data/src/main/java/com/clientmanager/model/Visit.java
@@ -0,0 +1,34 @@
+package com.clientmanager.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity{
+    private LocalDate date;
+    private Pet pet;
+    private String description;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
