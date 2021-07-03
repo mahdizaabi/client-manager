d72461c94853e42de37e545f8fa1d2343608fb68
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-03 03:19:25 +0200

    update vet controller and list vet list. Closes #9

--- client-manager-web/src/main/java/com/clientmanager/controllers/VetController.java
+++ client-manager-web/src/main/java/com/clientmanager/controllers/VetController.java
@@ -1,17 +1,28 @@
 package com.clientmanager.controllers;
 
 
+import com.clientmanager.model.Vet;
 import com.clientmanager.services.VetService;
 import com.clientmanager.services.map.VetServiceMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Set;
+
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
     public String   getVets(Model model){
+        Set<Vet> vetList = vetService.findAll();
+        model.addAttribute("vetList", vetList);
         return "vets/index";
     }
 }
