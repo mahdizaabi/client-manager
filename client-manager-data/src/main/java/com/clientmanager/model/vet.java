e4da5db38e4cc8187f3bdaa9032922823824d0e4
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-09 23:28:25 +0200

    map vet to JPA entity. Closes #15

--- client-manager-data/src/main/java/com/clientmanager/model/vet.java
+++ client-manager-data/src/main/java/com/clientmanager/model/vet.java
@@ -1,11 +1,19 @@
 package com.clientmanager.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vet")
 public class Vet extends Person{
 
     /*  initialize the hashset to avoid null pointer*/
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_Specialities",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
