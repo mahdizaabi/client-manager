52f22d19fbdcd7dff3bc3b06ebcf5ce69b1f5fc8
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-09 21:02:56 +0200

    update base Entity with JPA config. Closes #13

--- client-manager-data/src/main/java/com/clientmanager/model/BaseEntity.java
+++ client-manager-data/src/main/java/com/clientmanager/model/BaseEntity.java
@@ -1,14 +1,21 @@
 package com.clientmanager.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+//@mappedSuperclass: don't map this class to the database(don't create table for Base Entity);
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
