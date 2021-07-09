ba9ec8cb447b3165cd1024a59fdf25107f807eab
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-09 23:03:39 +0200

    convert owner to JPA entity. Closes #14

--- client-manager-data/src/main/java/com/clientmanager/model/Owner.java
+++ client-manager-data/src/main/java/com/clientmanager/model/Owner.java
@@ -1,13 +1,22 @@
 package com.clientmanager.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name="owners")
 public class Owner extends Person{
-    private Set<Pet> pets  =new HashSet<Pet>();
+
+    @Column(name= "adresse")
     private String adresse;
+    @Column(name="city")
     private String city;
+    @Column(name="telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private Set<Pet> pets  = new HashSet<>();
 
     public String getAdresse() {
         return adresse;
--- client-manager-data/src/main/java/com/clientmanager/model/Pet.java
+++ client-manager-data/src/main/java/com/clientmanager/model/Pet.java
@@ -1,11 +1,23 @@
 package com.clientmanager.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
 public class Pet extends BaseEntity{
+
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name="type_id")
     private PetType petType;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthdate;
 
     public void setPetType(PetType petType) {
