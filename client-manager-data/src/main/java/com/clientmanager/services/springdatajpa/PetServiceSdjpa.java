c47bb0206297b69aa09971bb5805f20e91330521
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-10 02:37:23 +0200

    add pet service SDJPA

---
+++ client-manager-data/src/main/java/com/clientmanager/services/springdatajpa/PetServiceSdjpa.java
@@ -0,0 +1,48 @@
+package com.clientmanager.services.springdatajpa;
+
+import com.clientmanager.model.Owner;
+import com.clientmanager.model.Pet;
+import com.clientmanager.model.PetType;
+import com.clientmanager.repositories.PetRepository;
+import com.clientmanager.repositories.PetTypeRepository;
+import com.clientmanager.services.PetService;
+import com.clientmanager.services.PetTypeService;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+public class PetServiceSdjpa implements PetService {
+    private final PetRepository petRepository;
+
+    public PetServiceSdjpa(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> petList = new HashSet<>();
+        petRepository.findAll().forEach(petList::add);
+        return petList;    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        Optional<Pet> owner = petRepository.findById(aLong);
+        return owner.orElse(null);    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+}
---
+++ client-manager-data/src/main/java/com/clientmanager/services/springdatajpa/PetTypeServiceSdjpa.java
@@ -0,0 +1,50 @@
+package com.clientmanager.services.springdatajpa;
+
+import com.clientmanager.model.Owner;
+import com.clientmanager.model.Pet;
+import com.clientmanager.model.PetType;
+import com.clientmanager.repositories.PetTypeRepository;
+import com.clientmanager.services.PetService;
+import com.clientmanager.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class PetTypeServiceSdjpa implements PetTypeService {
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeServiceSdjpa(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+
+    @Override
+    public Set<PetType> findAll() {
+
+        Set<PetType> petType = new HashSet<>();
+        petTypeRepository.findAll().forEach(petType::add);
+        return petType;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+}
