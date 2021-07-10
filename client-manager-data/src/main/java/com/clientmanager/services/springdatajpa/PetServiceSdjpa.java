e4e6cbb30eccde849ca3e6bd5a3a802df18c5ec6
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-10 02:42:54 +0200

    Speciality sdjpa ilementation

--- client-manager-data/src/main/java/com/clientmanager/services/springdatajpa/PetServiceSdjpa.java
+++ client-manager-data/src/main/java/com/clientmanager/services/springdatajpa/PetServiceSdjpa.java
@@ -7,11 +7,15 @@ import com.clientmanager.repositories.PetRepository;
 import com.clientmanager.repositories.PetTypeRepository;
 import com.clientmanager.services.PetService;
 import com.clientmanager.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
+@Service
+@Profile("springDataJpa")
 public class PetServiceSdjpa implements PetService {
     private final PetRepository petRepository;
 
---
+++ client-manager-data/src/main/java/com/clientmanager/services/springdatajpa/VetSpecialityServiceSdjpa.java
@@ -0,0 +1,48 @@
+package com.clientmanager.services.springdatajpa;
+
+import com.clientmanager.model.Owner;
+import com.clientmanager.model.Speciality;
+import com.clientmanager.repositories.SpecialityRepository;
+import com.clientmanager.services.VetSpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springDataJpa")
+public class VetSpecialityServiceSdjpa implements VetSpecialityService {
+    private final SpecialityRepository specialityRepository;
+
+    public VetSpecialityServiceSdjpa(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialitySet = new HashSet<>();
+        specialityRepository.findAll().forEach(specialitySet::add);
+        return specialitySet;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+}
